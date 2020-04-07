ce26b92f2ef4fdefc4c65f18b3da81e83d87e9e3
clm1010 <limin_chu@163.com>
2020-04-08 00:03:02 +0800

    搜索分类组件部分开发

--- src/pages/search/search.vue
+++ src/pages/search/search.vue
@@ -7,14 +7,17 @@
       @onTagClick="onTagClick"
       @onBtnClick="onBtnClick"
     />
+    <SearchItem icon="apps-o" title="计算机科学" sub-title="类别" />
   </div>
 </template>
 
 <script>
 import TagGroup from '@/components/base/TagGroup'
+import SearchItem from '@/components/search/SearchItem'
 export default {
   components: {
-    TagGroup
+    TagGroup,
+    SearchItem
   },
   data() {
     return {
