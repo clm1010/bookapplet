b534a3780ec4afdb664b44484ede2d5a56b1acb1
CLM10100 <limin_chu@163.com>
2020-04-21 17:03:07 +0800

    综合搜索列表开发

---
+++ src/components/search/SearchList.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="search-list-wrapper">
+    <SearchItem />
+    <SearchTable :data="list" />
+  </div>
+</template>
+
+<script>
+import SearchItem from './SearchItem'
+import SearchTable from './SearchTable'
+export default {
+  comments: {
+    SearchItem,
+    SearchTable
+  },
+  props: {
+    data: Object
+  },
+  data() {
+    list: [
+      {
+        id: 225,
+        fileName: '2016_Book_MicrofinanceEUStructuralFundsA',
+        cover:
+          'https://www.youbaobao.xyz/book/res/img/Economics/2016_Book_MicrofinanceEUStructuralFundsA.jpeg',
+        title:
+          'Microfinance, EU Structural Funds and Capacity Building for Managing Authorities',
+        author: 'Giovanni Nicola Pes',
+        publisher: 'Palgrave Macmillan',
+        bookId: '2016_Book_MicrofinanceEUStructuralFundsA',
+        category: 3,
+        categoryText: 'Economics',
+        language: 'en',
+        rootFile: 'OEBPS/9781137536013.opf'
+      },
+      {
+        id: 88,
+        fileName: '2018_Book_BetweenMobilityAndMigration',
+        cover:
+          'https://www.youbaobao.xyz/book/res/img/SocialSciences/978-3-319-77991-1_CoverFigure.jpg',
+        title: 'Between Mobility and Migration',
+        author: 'Peter Scholten',
+        publisher: 'Springer International Publishing',
+        bookId: '2018_Book_BetweenMobilityAndMigration',
+        category: 2,
+        categoryText: 'SocialSciences',
+        language: 'en',
+        rootFile: 'OEBPS/package.opf'
+      },
+      {
+        id: 24,
+        fileName: '2018_Book_SecurityInComputerAndInformati',
+        cover:
+          'https://www.youbaobao.xyz/book/res/img/ComputerScience/978-3-319-95189-8_CoverFigure.jpg',
+        title: 'Security in Computer and Information Sciences',
+        author: 'Erol Gelenbe',
+        publisher: 'Springer International Publishing',
+        bookId: '2018_Book_SecurityInComputerAndInformati',
+        category: 1,
+        categoryText: 'ComputerScience',
+        language: 'en',
+        rootFile: 'OEBPS/package.opf'
+      }
+    ]
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
--- src/components/search/SearchTable.vue
+++ src/components/search/SearchTable.vue
@@ -15,7 +15,7 @@
         <div class="book-info-wrapper">
           <div class="book-title">{{ book.title }}</div>
           <div class="book-author">{{ book.author }}</div>
-          <div class="book-category">{{ book.category }}</div>
+          <div class="book-category">{{ book.categoryText }}</div>
         </div>
       </div>
     </div>
@@ -49,7 +49,7 @@ export default {
   .search-table-inner {
     .search-table-book {
       border: 1px solid red;
-      margin: 15.5px 0;
+      margin: 14.5px 0;
       display: flex;
       align-items: center;
       .book-img-wrapper {
@@ -59,11 +59,28 @@ export default {
         }
       }
       .book-info-wrapper {
+        width: 80%;
+        margin-left: 15.5px;
         .book-title {
+          width: 100%;
+          font-size: 16px;
+          line-height: 22.5px;
+          font-weight: 500;
+          color: #333333;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
         }
         .book-author {
+          font-size: 13px;
+          line-height: 18px;
+          color: rgba(0, 0, 0, 0.65);
+          margin-top: 5px;
         }
         .book-category {
+          font-size: 13px;
+          line-height: 18px;
+          color: rgba(0, 0, 0, 0.45);
         }
       }
     }
