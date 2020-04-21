c6b5598440a48ec2dc17406e6cc00b13b8d17c8b
CLM10100 <limin_chu@163.com>
2020-04-21 15:10:40 +0800

    图书列表部分开发

---
+++ src/components/search/SearchTable.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="search-table-wrapper">
+    <div class="search-table-inner">
+      <div
+        class="search-table-book"
+        v-for="(book, index) in data"
+        :key="index"
+        @click="onClick(book)"
+      >
+        <div class="book-img-wrapper">
+          <div class="book-img">
+            <ImageView :src="book.cover" />
+          </div>
+        </div>
+        <div class="book-info-wrapper">
+          <div class="book-title">{{ book.title }}</div>
+          <div class="book-author">{{ book.author }}</div>
+          <div class="book-category">{{ book.category }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ImageView from '@/components/base/ImageView'
+export default {
+  name: 'SearchTable',
+  components: {
+    ImageView
+  },
+  props: {
+    data: Array
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    onClick(book) {
+      this.$emit('onClick', book)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.search-table-wrapper {
+  padding: 0 16px;
+  .search-table-inner {
+    .search-table-book {
+      border: 1px solid red;
+      margin: 15.5px 0;
+      display: flex;
+      align-items: center;
+      .book-img-wrapper {
+        .book-img {
+          width: 47px;
+          height: 68.5px;
+        }
+      }
+      .book-info-wrapper {
+        .book-title {
+        }
+        .book-author {
+        }
+        .book-category {
+        }
+      }
+    }
+  }
+}
+</style>
--- src/pages/search/search.vue
+++ src/pages/search/search.vue
@@ -1,27 +1,75 @@
 <template>
   <div>
-    <TagGroup
+    <!-- <TagGroup
       header-text="热门搜索"
       btn-text="换一批"
       :value="tags"
       @onTagClick="onTagClick"
       @onBtnClick="onBtnClick"
     />
-    <SearchItem icon="apps-o" title="计算机科学" sub-title="类别" />
+    <SearchItem icon="apps-o" title="计算机科学" sub-title="类别" /> -->
+    <SearchTable :data="list" />
   </div>
 </template>
 
 <script>
 import TagGroup from '@/components/base/TagGroup'
 import SearchItem from '@/components/search/SearchItem'
+import SearchTable from '@/components/search/SearchTable'
 export default {
   components: {
     TagGroup,
-    SearchItem
+    SearchItem,
+    SearchTable
   },
   data() {
     return {
-      tags: ['aaaaa', 'bbbbb', 'ccccc', 'ddddddddddddddddddddddd', 'eeeeeeeee']
+      // tags: ['aaaaa', 'bbbbb', 'ccccc', 'ddddddddddddddddddddddd', 'eeeeeeeee']
+      list: [
+        {
+          id: 225,
+          fileName: '2016_Book_MicrofinanceEUStructuralFundsA',
+          cover:
+            'https://www.youbaobao.xyz/book/res/img/Economics/2016_Book_MicrofinanceEUStructuralFundsA.jpeg',
+          title:
+            'Microfinance, EU Structural Funds and Capacity Building for Managing Authorities',
+          author: 'Giovanni Nicola Pes',
+          publisher: 'Palgrave Macmillan',
+          bookId: '2016_Book_MicrofinanceEUStructuralFundsA',
+          category: 3,
+          categoryText: 'Economics',
+          language: 'en',
+          rootFile: 'OEBPS/9781137536013.opf'
+        },
+        {
+          id: 88,
+          fileName: '2018_Book_BetweenMobilityAndMigration',
+          cover:
+            'https://www.youbaobao.xyz/book/res/img/SocialSciences/978-3-319-77991-1_CoverFigure.jpg',
+          title: 'Between Mobility and Migration',
+          author: 'Peter Scholten',
+          publisher: 'Springer International Publishing',
+          bookId: '2018_Book_BetweenMobilityAndMigration',
+          category: 2,
+          categoryText: 'SocialSciences',
+          language: 'en',
+          rootFile: 'OEBPS/package.opf'
+        },
+        {
+          id: 24,
+          fileName: '2018_Book_SecurityInComputerAndInformati',
+          cover:
+            'https://www.youbaobao.xyz/book/res/img/ComputerScience/978-3-319-95189-8_CoverFigure.jpg',
+          title: 'Security in Computer and Information Sciences',
+          author: 'Erol Gelenbe',
+          publisher: 'Springer International Publishing',
+          bookId: '2018_Book_SecurityInComputerAndInformati',
+          category: 1,
+          categoryText: 'ComputerScience',
+          language: 'en',
+          rootFile: 'OEBPS/package.opf'
+        }
+      ]
     }
   },
   methods: {
